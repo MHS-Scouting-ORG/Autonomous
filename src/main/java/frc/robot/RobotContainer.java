237e7c7856b81f161de9dee6eb99006443d1aed9
KeaniKina <keani1028@gmail.com>
2023-02-20 11:51:01 -1000

    Added auto commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,13 +2,14 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  
+  private final SwerveSubsystem swerve = new SwerveSubsystem();
 
   public RobotContainer() {
     configureBindings();
@@ -18,6 +19,6 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return null;
+    return Autos.Auto(swerve);
   }
 }
