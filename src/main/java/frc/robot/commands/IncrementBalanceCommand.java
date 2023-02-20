0f7cfdbe988ad83d87b3bf4356b25bcad9b7da29
joviTerawaki <jovi.terawaki2025@mauihigh.org>
2023-02-20 10:37:23 -1000

    increment balance cmd created; not tested

--- src/main/java/frc/robot/commands/IncrementBalanceCommand.java
+++ src/main/java/frc/robot/commands/IncrementBalanceCommand.java
@@ -5,14 +5,19 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.commands.MovementCommands.DriveForwardCommand;
+import frc.robot.commands.MovementCommands.DriveBackwardCommand;
 
 public class IncrementBalanceCommand extends CommandBase {
   private final SwerveSubsystem swerve; 
   private Timer timer; 
+  private Timer autoTimer; 
+  private double prevPitch; 
+  private double currentPitch; 
 
   public IncrementBalanceCommand(SwerveSubsystem newSwerve) {
     swerve = newSwerve; 
     timer = new Timer(); 
+    autoTimer = new Timer(); 
 
     addRequirements(swerve);
   }
@@ -20,12 +25,21 @@ public class IncrementBalanceCommand extends CommandBase {
   @Override
   public void initialize() {
   timer.reset();
+  timer.start();
+  autoTimer.start();
+
+  prevPitch = 0; 
   }
 
   @Override
   public void execute() {
-    //dummy value
-    new DriveForwardCommand(swerve, 100); 
+    if (timer.get() >= 0.5) {
+      new DriveBackwardCommand(swerve, 100); 
+      timer.reset();
+    } 
+
+    prevPitch = currentPitch; 
+    currentPitch = swerve.getRoll();
   }
 
   @Override
@@ -35,6 +49,7 @@ public class IncrementBalanceCommand extends CommandBase {
 
   @Override
   public boolean isFinished() {
-    return false;
+    /*navx reads drop or autonomous period ends  */
+    return Math.abs(currentPitch - prevPitch) >= 5 || autoTimer.get() >= 14.5; //change timer bool later  if need
   }
 }
