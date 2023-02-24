c857a6b7fe70c45b99ff4b2b2de4ad73b9527280
joviTerawaki <jovi.terawaki2025@mauihigh.org>
2023-02-23 18:41:43 -1000

    MidHigh class created. commented movements

--- src/main/java/frc/robot/commands/AutonomousCommands/Hybrid.java
+++ src/main/java/frc/robot/commands/AutonomousCommands/Hybrid.java
@@ -5,9 +5,9 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class SideHybrid extends SequentialCommandGroup {
+public class Hybrid extends SequentialCommandGroup {
   /** Creates a new SideHybrid. */
-  public SideHybrid() {
+  public Hybrid() {
     addCommands(
     // start lined up with community edge 
     // bring elev up and pivot out 
