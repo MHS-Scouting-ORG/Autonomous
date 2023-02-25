b5165755fe4441426775224153b8268c3aa679c7
joviTerawaki <jovi.terawaki2025@mauihigh.org>
2023-02-25 13:26:41 -1000

    prepared to write High and Hybrid

--- src/main/java/frc/robot/commands/AutonomousCommands/High.java
+++ src/main/java/frc/robot/commands/AutonomousCommands/High.java
@@ -2,11 +2,12 @@ package frc.robot.commands.AutonomousCommands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class Hybrid extends SequentialCommandGroup {
+public class High extends SequentialCommandGroup {
 
-  public Hybrid() {
+  public High() {
+
     addCommands(
-    
+
     );
   }
 }
