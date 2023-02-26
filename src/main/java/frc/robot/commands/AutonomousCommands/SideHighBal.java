43267b1a010a294d7d917c04ca912a4ed394b5b3
KeaniKina <keani1028@gmail.com>
2023-02-25 14:51:54 -1000

    Added autonomous sequential commands

---
+++ src/main/java/frc/robot/commands/AutonomousCommands/SideHighBal.java
@@ -0,0 +1,12 @@
+package frc.robot.commands.AutonomousCommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+
+public class SideHighBal extends SequentialCommandGroup {
+
+  public SideHighBal() {
+    
+    addCommands();
+  }
+}
