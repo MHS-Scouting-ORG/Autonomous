a88537258f94a6e4940ba2cd083c05a511a017b1
joviTerawaki <jovi.terawaki2025@mauihigh.org>
2023-02-25 15:29:53 -1000

    added claw toggle cmd

---
+++ src/main/java/frc/robot/commands/ClawCommands/Claw.java
@@ -0,0 +1,30 @@
+package frc.robot.commands.ClawCommands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ClawSubsystem;
+
+//TOGGLE CLAW 
+public class Claw extends CommandBase {
+  private ClawSubsystem clawSubsystem;
+
+  public Claw(ClawSubsystem claw) {
+    clawSubsystem = claw;
+    addRequirements(claw);
+  }
+
+  @Override
+  public void initialize() {
+    clawSubsystem.toggle();
+  }
+
+  @Override
+  public void execute() {}
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
